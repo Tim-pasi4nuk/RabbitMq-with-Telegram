6f136536fa0fe64e35adf05143804c4d2120d5f3
Tim Pasichnuk <timkapva13@gmail.com>
2022-08-19 17:02:10 +0300

    created microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TelegrafModule } from 'nestjs-telegraf';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { TelegramModule } from './telegram/telegram.module';
+require('dotenv').config();
 
 @Module({
-  imports: [],
+  imports: [RabbitmqModule, TelegramModule],
   controllers: [AppController],
   providers: [AppService],
 })
